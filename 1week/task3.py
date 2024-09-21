38c3cfac4986f560d80b1b3d180c46f76f1f8cae
userxg <137679548+userxg@users.noreply.github.com>
2024-09-21 15:26:30 +0300

    1week

---
+++ 1week/task3.py
@@ -0,0 +1,19 @@
+'''
+leetcode.com/problem-list/string/
+https://leetcode.com/problems/generate-parentheses/description/
+'''
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        n -= 1
+        result = {"()"}
+
+        while n > 0:
+            newResult = set()
+            for item in result:
+                for i in range(len(item)):
+                    newResult.add(item[:i] + "()" + item[i:])
+            result = newResult
+            n -= 1
+        return result
---
+++ 1week/task5.py
@@ -0,0 +1,35 @@
+
+'''
+leetcode.com/problem-list/string/
+https://leetcode.com/problems/integer-to-roman/description/
+'''
+
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        res = ''
+        dict = [
+          [1 , "I"],
+          [4 , "IV"],
+          [5 , "V"],
+            [9 , "IX"],
+            [10 , "X"],
+           [40 , "XL"] ,
+           [50 , "L"],
+           [90 , "XC"],
+            [100 , "C"],
+            [400 , "CD"],
+            [500 , "D"],
+            [900 , "CM"],
+            [1000, "M"]
+        ]
+        
+        i = 12
+
+        while (num > 0):
+            if (num >= dict[i][0]):
+                res += dict[i][1]
+                num -= dict[i][0]
+            else:
+                i -= 1
+        return res
